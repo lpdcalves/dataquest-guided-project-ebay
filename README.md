c80a60243e801b45f5628fbddf8b4ea386fde8fb
Luiz P C Alves <luiz_paulo.09@hotmail.com>
2021-11-10 08:43:17 -0300

    Finishing up the project

--- README.md
+++ README.md
@@ -2,6 +2,29 @@
 
 This repo contains an implementation of Dataquest's [Exploring eBay Car Sales](https://app.dataquest.io/c/54/m/294/guided-project%3A-exploring-ebay-car-sales-data/) Data Guided Project.
 
-This project was initially developed in a jupyter notebook, and then transformed into a python script, both of which are available in the repo.
+This project was initially developed in a jupyter notebook, and then transformed into a python script, both of which are stored here. The main goal of this project was to practice the use of clean code technologies, such as pylint, on top of datascience applications.
 
-TODO: commands used to check (pylint) and adapt (pep8) the code.
+## Technologies Used
+
+A few of the most notorious and widespread tools for datascience and code cleaning were utilized, such as:
+- Jupyter
+- Pandas
+- Pylint
+
+## Code Cleanliness
+
+Pylint was used to validate the cleanliness of the code, achieving a score of 10/10.
+
+To run pylint simply install it using pip:
+```
+pip install pylint
+```
+And then run our file through it:
+```
+pylint carsales.py
+```
+If your code is up to [PEP8](https://www.python.org/dev/peps/pep-0008/) standards, your pylint output should look similar to this:
+```
+-------------------------------------------------------------------
+Your code has been rated at 10.00/10 (previous run: 8.36/10, +1.64)
+```
--- carsales.py
+++ carsales.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+# pylint: disable=no-member
+"""
+This file implements Dataquest's Exploring eBay Car Sales guided project in a python script.
+This was first written in a jupyter notebook, and as such, it may look a bit odd on a .py file.
+"""
 
-print("This is the implementation of Dataquest's \"Exploring eBay Car Sales Data\" guided project, which, as the name suggests, uses a dataset of eBay car listings in Germany.")
-
-import numpy as np
-import pandas as pd
 import os
+import pandas as pd
+
+print("This is an implementation of Dataquest's \"Exploring eBay Car Sales Data\" guided project,")
+print("which, as the name suggests, uses a dataset of eBay car listings in Germany.")
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'autos.csv')
 
 autos = pd.read_csv(filename, encoding='Latin-1')
 
-print("Informações Gerais do dataset:")
+print("General dataset info:")
 print(autos.info())
 print("Head do dataset:")
 print(autos.head())
 
 
-print("Observations: The dataset seems to be mostly intact. Most of the columns contain the full 50K entries, but a few of them such as \"notRepairedDamage\" and \"vehicleType\" are missing quite a few thousand entries.")
+print("Observations: The dataset seems to be mostly intact. Most of the columns")
+print("contain the full 50K entries, but a few of them")
+print("such as \"notRepairedDamage\" and \"vehicleType\" are missing quite a few thousand entries.")
 #Cleaning Column Names
 
 autos.columns = ['date_crawled', 'name', 'seller', 'offer_type', 'price', 'abtest',
@@ -29,13 +34,17 @@ autos.columns = ['date_crawled', 'name', 'seller', 'offer_type', 'price', 'abtes
 
 print(autos.head())
 
-print("In this section we basically just cleaned up the name of the columns without altering any data.")
+print("In this section we basically just cleaned up the columns' names without altering any data.")
 print("Data Exploration")
 
 print(autos.describe(include='all'))
 
-print("Apparently the columns \"seller\", \"offer_type\", \"abtest\", \"vehicle_type\", \"gearbox\" and \"odometer\" all have very low numbers of unique entries, which may indicate they are good candidates for being dropped.")
-print("Strangely, \"registration_year\", \"registration_month\", \"nr_of_pictures\" and \"postal_code\" seem to return NAN, which is counter intuitive.")
+print("Apparently the columns \"seller\", \"offer_type\", \"abtest\", \"vehicle_type\",")
+print(" \"gearbox\" and \"odometer\" all have very low numbers of unique entries,")
+print("which may indicate they are good candidates for being dropped.")
+
+print("Strangely, \"registration_year\", \"registration_month\", \"nr_of_pictures\" and")
+print("\"postal_code\" seem to return NAN, which is counter intuitive.")
 print("Let's investigate this further.")
 
 print(autos.registration_year.value_counts())
@@ -43,13 +52,18 @@ print(autos.nr_of_pictures.value_counts())
 print(autos.postal_code.value_counts())
 print(autos.registration_month.value_counts())
 
-print("Analyzing theses results we can tell that, appart from \"nr_of_pictures\", all other columns with NAN in unique actually have many distinct values. For some reason describe() isn't being able to parse this data correctly. Therefore, we are going to drop \"nr_of_pictures\".")
+print("Analyzing theses results we can tell that, appart from \"nr_of_pictures\",")
+print("all other columns with NAN in unique actually have many distinct values.")
+print("For some reason describe() isn't being able to parse this data correctly.")
+print("Therefore, we are going to drop \"nr_of_pictures\".")
 
 autos = autos.drop(["nr_of_pictures"], axis=1)
 print(autos.head())
 print(autos.dtypes)
 
-print("From the code above we can tell which columns are objects (in this case strings) and which are integer numbers. We wish to change \"price\" and \"odometer\" from strings to numbers.")
+print("From the code above we can tell which columns are objects (in this case strings)")
+print("and which are integer numbers. We wish to change \"price\" and")
+print("\"odometer\" from strings to numbers.")
 
 autos.price = (autos.price
                     .str.replace("$","")
@@ -65,7 +79,8 @@ autos.rename({"odometer" : "odometer_km"}, axis=1, inplace=True)
 print(autos.odometer_km.head())
 
 print("Continuing Exploration of Columns")
-print("We will continue exploring odometer_km and price to try and find outliers so we can remove them.")
+print("We will continue exploring odometer_km and price to try and find")
+print("outliers so we can remove them.")
 
 print(autos.odometer_km.unique().shape)
 print(autos.price.unique().shape)
@@ -73,11 +88,16 @@ print(autos.odometer_km.describe())
 print(autos.price.describe())
 print(autos.odometer_km.value_counts().sort_index(ascending=True))
 
-print("There doesn't seem to be anything unusual with the ranges of the odometer_km column, so we will leave it as is.")
+print("There doesn't seem to be anything unusual with the ranges of")
+print("the odometer_km column, so we will leave it as is.")
 
 print(autos.price.value_counts().sort_index(ascending=True))
 
-print("It seems there are a few cars both on the very low end (zero euros) and very high end (99 million euros) of prices. To fix this we could create a minimum and maximum value and then filter our price column to these values. I arbitrarily chose that 1 euro and 1 million euros would be the minimum and maximum, respectively.")
+print("It seems there are a few cars both on the very low end (zero euros)")
+print("and very high end (99 million euros) of prices.")
+print("To fix this we could create a minimum and maximum value and then filter")
+print("our price column to these values. I arbitrarily chose that 1 euro and")
+print("1 million euros would be the minimum and maximum, respectively.")
 
 autos = autos[autos.price.between(1, 1000000)]
 
@@ -90,27 +110,33 @@ print((autos.date_crawled
              .value_counts(normalize=True, dropna=False)
              .sort_index()))
 
-print("From exploring date_crawled we can gather that the crawling was made over roughly a month, from March 5th, 2016 to April 7th, 2016. Beyond that, there doesn't seem to be any day which had a lot more crawling done than any other.")
+print("From exploring date_crawled we can gather that the crawling was made")
+print("over roughly a month, from March 5th, 2016 to April 7th, 2016. Beyond that,")
+print("there doesn't seem to be any day which had a lot more crawling done than any other.")
 
 print((autos.last_seen
              .str[:10]
              .value_counts(normalize=True, dropna=False)
              .sort_index()))
 
-print("The last_seen column is pretty similar to date_crawled, even starting and ending on the same days.")
+print("The last_seen column is pretty similar to date_crawled,")
+print("even starting and ending on the same days.")
 
 print((autos.ad_created
              .str[:10]
              .value_counts(normalize=True, dropna=False)
              .sort_index()))
 
-print("ad_created is a little bit different from the two before, since it has some older listings, going back as far as the 11th of june, 2015.")
+print("ad_created is a little bit different from the two before, since it has some")
+print("older listings, going back as far as the 11th of june, 2015.")
 
 print("Now, exploring the distribution of registration_year:")
 
 print(autos.registration_year.describe())
 
-print("From this result we can straight up tell that some entries are wrong, since the minimum year is 1000, and the maximum is 9999, which shows we need to clean up this column's data. Let's find out how many of our entries lie beyond 1900-2016:")
+print("From this result we can straight up tell that some entries are wrong, since the")
+print("minimum year is 1000, and the maximum is 9999, which shows we need to clean")
+print("up this column's data. Let's find out how many of our entries lie beyond 1900-2016:")
 
 print((~autos["registration_year"].between(1900,2016)).sum() / autos.shape[0])
 
@@ -120,13 +146,16 @@ autos = autos[autos["registration_year"].between(1900,2016)]
 
 print(autos["registration_year"].value_counts(normalize=True))
 
-print("From the percentage of each year, aquired above, it seems most cars were registere in the past 30 or so years.")
+print("From the percentage of each year, aquired above, it seems")
+print("most cars were registere in the past 30 or so years.")
 
 print("Exploring Brands")
 
 print(autos.brand.value_counts())
 
-print("We can see that the top 5 most popular car brands are german, which is kind of fitting since theses listings are from German eBay. Anyway, we will compare the average pricing from the top 5 brands to the bottom 5 brands:")
+print("We can see that the top 5 most popular car brands are german, which is kind of fitting")
+print("since theses listings are from German eBay. Anyway, we will compare the average pricing")
+print("from the top 5 brands to the bottom 5 brands:")
 
 top_5_brands = ['volkswagen', 'bmw', 'opel', 'mercedes_benz', 'audi']
 
@@ -148,7 +177,10 @@ for brand in bottom_5_brands:
 
 print(bottom_5_mean_prices)
 
-print("Comparing the top brands and bottom brands we can infer that the consumer preference doesn't come from better pricing, since almost all top 5 brands (except OPEL) have higher average prices than the bottom 5. Therefore, the german market preference might come from product quality or other less objective reasons such as taste and culture.")
+print("Comparing the top brands and bottom brands we can infer that the consumer preference")
+print("doesn't come from better pricing, since almost all top 5 brands (except OPEL) have higher")
+print("average prices than the bottom 5. Therefore, the german market preference might come from")
+print("product quality or other less objective reasons such as taste and culture.")
 
 print("Exploring Mean Mileage from Brands")
 
@@ -167,4 +199,6 @@ brand_info = pd.DataFrame(mean_mileage, columns=['mean_mileage'])
 brand_info["mean_price"] = mean_prices
 print(brand_info)
 
-print("There doesn't seem to be any relation between average mileage and average car price from what we can gather in this table. Overall, the conclusion we could achieve is that BMWs, Mercedes and Audis are just more expensive cars.")
+print("There doesn't seem to be any relation between average mileage and average")
+print("car price from what we can gather in this table. Overall, the conclusion we")
+print("could achieve is that BMWs, Mercedes and Audis are just more expensive cars.")
